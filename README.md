5b0f913176e16692dbf36cbb8cee9619367d3bb5
Tony Merlin <code@what-the-functor.com>
2024-02-03 18:21:21 +0800

    Nix flake with typescript-language-server

---
+++ README.md
@@ -0,0 +1,20 @@
+# Nix TypeScript
+
+A minimal TypeScript and JavaScript development environment as a Nix flake, based on:
+- typescript language server
+
+## Usage
+
+### Ad-hoc shell
+
+```shell
+nix develop github:what-the-functor/nix-typescript
+```
+
+### direnv
+
+`.envrc`
+```
+use flake github:what-the-functor/nix-typescript
+```
+
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Typescript development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+      in {
+        devShells = {
+
+          # Default environment for typescript and javascript development
+          default = pkgs.mkShell {
+            packages = [ pkgs.nodePackages.typescript-language-server ];
+          };
+
+          # Nix environment for development of this flake
+          # Additionally pandoc is used for markdown support
+          # See .envrc at the root of this project
+          nix = pkgs.mkShell { packages = with pkgs; [ nil nixfmt pandoc ]; };
+        };
+      });
+}
